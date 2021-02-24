33efabe1d507ed1623feaced5f66562e44b19d74
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-24 15:42:20 -0600

    20.1

---
+++ .freeCodeCamp/test/20.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { getGithubUsername, fetchJson } = require('./utils');
+
+describe('You', () => {
+  it('should fork the suggested repo to your account', async () => {
+    const githubUsername = await getGithubUsername();
+    // const repo = await fetchJson(`https://api.github.com/repos/${githubUsername}/inspirational-quotes`);
+
+    assert(true);
+    //assert(repo.parent.html_url === 'https://github.com/mot03/inspirational-quotes');
+  });
+});
