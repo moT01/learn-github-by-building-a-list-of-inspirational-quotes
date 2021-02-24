bde2214390ccd693d48413cf7ddc4884219c1593
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-24 15:42:42 -0600

    30.1

--- .freeCodeCamp/test/20.test.js
+++ .freeCodeCamp/test/20.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getGithubUsername, fetchJson } = require('./utils');
 
 describe('You', () => {
@@ -9,4 +9,4 @@ describe('You', () => {
     assert(true);
     //assert(repo.parent.html_url === 'https://github.com/mot03/inspirational-quotes');
   });
-});
+});*/
---
+++ .freeCodeCamp/test/30.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getGithubUsername } = require('./utils');
+
+describe('You', () => {
+  it('should correctly clone your repo to this machine', async () => {
+    const lastCommand = await getLastCommand();
+    const githubUsername = await getGithubUsername();
+    const cwd = await getCwd();
+
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'clone' && lastCommand[2] === `https://github.com/${githubUsername}/inspirational-quotes.git` && cwd === '/home/strove/project');
+  });
+});
