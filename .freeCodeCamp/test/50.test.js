c0f71c1d4d93a6e6952c5180a512fbd0e545613f
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-26 14:46:35 -0600

    50.1

---
+++ .freeCodeCamp/test/50.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  it('should change into the correct directory', async () => {
+    const lastCommand = await getLastCommand();
+    const cwd = await getCwd();
+
+    assert(lastCommand[0] === 'cd' && cwd === '/home/strove/project/inspirational-quotes');
+  });
+});
