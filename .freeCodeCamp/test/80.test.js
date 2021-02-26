1be3047f77128845d67e2a4a0736001324094607
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-26 14:53:35 -0600

    80.1

---
+++ .freeCodeCamp/test/80.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  it('should view the branches in your repository with the correct flag', async () => {
+    const lastCommand = await getLastCommand();
+    const cwd = await getCwd();
+    const lc2 = lastCommand[2] === '-a' || lastCommand[2] === '--all';
+
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'branch' && lc2 && cwd === '/home/strove/project/inspirational-quotes');
+  });
+});
