fd0b93b473970c41592bdff986d80573d2aa3211
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-26 14:54:33 -0600

    90.1

---
+++ .freeCodeCamp/test/90.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  it('should view the remotes your repository has', async () => {
+    const lastCommand = await getLastCommand();
+    const cwd = await getCwd();
+
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'remote' && lastCommand[2] === undefined && cwd === '/home/strove/project/inspirational-quotes');
+  });
+});
