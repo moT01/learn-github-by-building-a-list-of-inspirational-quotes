e24af7f05d202b650d128aa4d753048cd9d74535
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-26 15:22:39 -0600

    190.1

---
+++ .freeCodeCamp/test/190.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  it('should view your git config with the correct flag', async () => {
+    const lastCommand = await getLastCommand();
+    const cwd = await getCwd();
+    const lc2 = lastCommand[2] === '-l' || lastCommand[2] === '--list';
+
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'config' && lc2 && cwd === '/home/strove/project/inspirational-quotes');
+  });
+});
