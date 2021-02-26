2299c464b327f196982436e81dd9f0730536a7dc
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-26 15:58:40 -0600

    290.1

---
+++ .freeCodeCamp/test/290.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { getCommits, getGitHubUsername } = require('./utils');
+
+describe('You', () => {
+  it('should commit the staged files with the correct message', async () => {
+    const commits = await getCommits();
+    const githubUsername = await getGitHubUsername();
+
+    assert(commits[0].message === `feat: add ${githubUsername}'s first quote`);
+  });
+});
