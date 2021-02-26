f8fafa68be7706f88b9bf247894980b00d78ad62
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-26 16:01:32 -0600

    320.1

--- .freeCodeCamp/test/320.test.js
+++ .freeCodeCamp/test/320.test.js
@@ -1,12 +1,12 @@
-/*const assert = require('assert');
+const assert = require('assert');
 const { getLastCommand, getCwd, getGitHubUsername } = require('./utils');
 
 describe('You', () => {
-  it('should push your code to github', async () => {
+  it('should correctly create a PR to the parent repo', async () => {
     const githubUsername = await getGitHubUsername();
     const lastCommand = await getLastCommand();
     const cwd = await getCwd();
 
     assert(false);
   });
-});*/
+});
