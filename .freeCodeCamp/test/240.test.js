e2c44e4f9d2303de609e85b17f21601ecb014193
moT01 <20648924+moT01@users.noreply.github.com>
2021-02-26 15:53:12 -0600

    250.1

--- .freeCodeCamp/test/240.test.js
+++ .freeCodeCamp/test/240.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getLastCommand, getDirectoryContents, getGitHubUsername, getCwd } = require('./utils');
 
 describe('You', () => {
@@ -11,4 +11,4 @@ describe('You', () => {
 
     assert(userDirectoryContents.includes('my_quotes.sh') && lastCommand[0] === 'touch' && re.test(lastCommand[1]) && cwd === '/home/strove/project/inspirational-quotes');
   });
-});
+});*/
---
+++ .freeCodeCamp/test/250.test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('You', () => {
+  it('should add a quote and author to your JSON file', async () => {
+    assert(false);
+  });
+});
